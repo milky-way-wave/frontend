51e6b063ba1915e573834f075e56b2002e1435f3
acayseth <gorbulea.s@gmail.com>
2025-08-22 23:32:04 +0300

    codeman #101

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,31 @@ import withRspack from "next-rspack";
 
 import type { NextConfig } from "next";
 
+const uId: string = `${+new Date()}`;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: process.env.NODE_ENV === "development",
+  output: "standalone",
+  cleanDistDir: true,
+  images: {
+    unoptimized: true,
+  },
+  generateBuildId: () => uId,
+  deploymentId: uId,
+  compiler:
+    process.env.NODE_ENV === "development"
+      ? undefined
+      : {
+          removeConsole: {
+            exclude: ["error"],
+          },
+        },
+  logging: {
+    fetches: {
+      fullUrl: process.env.NODE_ENV === "development",
+    },
+  },
+  devIndicators: false,
 };
 
 export default withRspack(nextConfig);
